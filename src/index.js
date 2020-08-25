f709c14d1787105b51223d047b84f86f16445a78
Reese <arter.charise@gmail.com>
2020-08-26 07:26:24 +0900

    Mostly works
    random seed, fast, slow, clear, pause, play work but not when I try to add my own seeds.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, DropdownButton, Dropdown } from 'react-bootstrap'
 
 //Box component
 
@@ -51,7 +52,37 @@ class Grid extends React.Component {
   }
 } // End Grid Component
 
+//Buttons Component
+class Buttons extends React.Component {
 
+  //handle select function
+  handleSelect = (e) => {
+    this.props.gridSize(e)
+  }
+  render() {
+    return (
+      <div className = 'center'>
+        <ButtonToolbar>
+          <button classname="btn btn-default" onClick={this.props.playButton}> Play </button>
+          <button classname="btn btn-default" onClick={this.props.pauseButton}> Pause </button>
+          <button classname="btn btn-default" onClick={this.props.slow}> Slow </button>
+          <button classname="btn btn-default" onClick={this.props.fast}> Fast </button>
+          <button classname="btn btn-default" onClick={this.props.seed}> Seed </button>
+          <button classname="btn btn-default" onClick={this.props.clear}> Clear </button>
+      {/* Work on buttun aesthetic for bootstrap*/}
+          <DropdownButton title='Grid Size' id='size-menu' onSelect={this.handleSelect}>
+          <Dropdown.Item eventKey='1'>20x10</Dropdown.Item>
+          <Dropdown.Item eventKey='2'>50x30</Dropdown.Item>
+          <Dropdown.Item eventKey='3'>70x50</Dropdown.Item>
+          </DropdownButton>
+        </ButtonToolbar>
+
+      </div>
+    )
+  }
+}
+
+//End Buttons Component
 
 
 //Main Component and Game Buttons
@@ -100,6 +131,50 @@ playButton = () => {
   this.intervalId = setInterval(this.play, this.speed)
 }
 
+//Pause button
+pauseButton = () => {
+  clearInterval(this.intervalId)
+}
+
+//Slow button
+slow = () => {
+  this.speed= 1000;
+  this.playButton();
+}
+
+//Fast Button
+fast = () => {
+  this.speed = 100;
+  this.playButton();
+}
+//Clear Button
+clear = () => {
+  let grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+  this.setState({
+    gridFull: grid,
+    generation: 0
+  });
+}
+
+//Grid Size in order of event key in drop down
+
+gridSize = (size) => {
+  switch (size) {
+    case "1":
+      this.cols = 20;
+      this.rows = 10;
+    break
+    case "2":
+      this.cols = 50;
+      this.rows = 30;
+    break
+    default:
+      this.cols = 70;
+      this.rows = 50;
+  }
+  this.clear();
+}
+
 //play method and game logic
 play = () => {
   let g = this.state.gridFull; //checks state of grid (1st copy)
@@ -125,7 +200,7 @@ play = () => {
   }
   this.setState({ //updates state of grid
     gridFull: g2,
-    generation: this.state.generation++ //go to next generation
+    generation: this.state.generation + 1 //go to next generation
   })
 }
 
@@ -141,7 +216,17 @@ componentDidMount() {
     return (
       <div>
         <h1> The Game of Life</h1>
-        {/* //create grid */}
+        {/* Create buttons here */}
+        <Buttons
+          playButton = {this.playButton}
+          pauseButton = {this.pauseButton}
+          slow = {this.slow}
+          fast = {this.fast}
+          clear = {this.clear}
+          seed = {this.seed}
+          gridSize = {this.gridSize}
+        />
+        {/* create grid */}
         <Grid 
         gridFull = {this.state.gridFull} //properties of gridFull
         rows = {this.rows} // properties of row
